6a0abd9e875821bc326552bb83c0951e340d6374
Carlos Rivera <riveramoralescarlosandres25@gmail.com>
2023-05-27 17:42:35 -0500

    se crea practica de spring boot

---
+++ src/main/java/com/example/platzispringalejo/PlatziSpringAlejoApplication.java
@@ -0,0 +1,13 @@
+package com.example.platzispringalejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziSpringAlejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziSpringAlejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/platzispringalejo/PlatziSpringAlejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platzispringalejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziSpringAlejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
